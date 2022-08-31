2743fe1ac39f76f6f250333616f79c5ee8b9f25d
bazijun <1324022569@qq.com>
2022-08-31 16:05:10 +0800

    脚本更新-01

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -4,13 +4,13 @@ dirName="nav";
 
 echo "--> yarn install"
 cd ..
-sudo yarn
+yarn
 
 echo "--> yarn build"
-sudo yarn build
+yarn build
 
 echo "--> resource update"
-sudo rm -rf /home/web/nav/*
-sudo cp -rf ./dist/* /home/web/nav/
+rm -rf /home/web/"$dirName"/*
+cp -rf ./dist/* /home/web/"$dirName"/
 
 echo " (๑•̀ㅂ•́)و✧ "
