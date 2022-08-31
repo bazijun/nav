da92de7a83b0e2f080259a8ba1b85c4e102bd687
bazijun <1324022569@qq.com>
2022-08-31 23:17:26 +0800

    部署测试

--- src/components/yu-terminal/YuTerminal.vue
+++ src/components/yu-terminal/YuTerminal.vue
@@ -405,7 +405,7 @@ onMounted(() => {
     });
   } else {
     terminal.writeTextOutput(
-      `😆小把汁导航` +
+      `😆小把子导航` +
         `<a href="//github.com/liyupi/yuindex" target='_blank'> thanks</a>`
     );
     terminal.writeTextOutput("<br/>");
